d7981ac18ce57fbc7dcbd518d35c8936015742bf
chaeyoo <chaeh0124@gmail.com>
2023-09-16 10:49:55 +0900

    [FEAT] bfs baekjoon 1697, 7562

---
+++ baekjoon/1697.js
@@ -0,0 +1,55 @@
+class Queue {
+    constructor() {
+        this.items = [];
+        this.headIndex = 0;
+        this.tailIndex = 0;
+    }
+    enqueue(item) {
+        this.items[this.tailIndex] = item;
+        this.tailIndex++;
+    }
+    dequeue() {
+        const item = this.items[this.headIndex];
+        delete this.items[this.headIndex];
+        this.headIndex++;
+        return item;
+    }
+    peek() {
+        return this.items[this.headIndex];
+    }
+    getLength() {
+        return this.tailIndex - this.headIndex;
+    }
+
+}
+
+const test = "5 17";
+const input = test.toString().split('\n');
+const MAX = 100001;
+let [n, k] = input[0].split(' ').map(Number);
+let visited = new Array(MAX).fill(0);
+
+function bfs() {
+    // process
+    // 1. 최초 노드 큐에 넣기 (방문처리 필요있으면 하고 아님말고)
+    // 2. 큐 길이가 0 될 때까지 디큐
+    // 3. 디큐하면서 인접노드들 반복문 (디큐했을 때 그 값이 목표값이라면 리턴 거리값)
+    // 4. 인접노드들의 범위가 유효한지 & 방문한적 있는지 체크
+    // 5. 해당없으면 방문처리하고 인큐 (방문처리는 해당 위치까지 거리 값이라는 의미가 있음)
+    const queue = new Queue();
+    queue.enqueue(n);
+    while (queue.getLength() !== 0) {
+        let cur = queue.dequeue();
+        if (cur == k) {
+            return visited[cur];
+        }
+        for (let next of [cur - 1, cur + 1, cur *2]) {
+            if (next < 0 || n >= MAX) continue;
+            if (visited[next] == 0) {
+                visited[next] = visited[cur] + 1; // 방문처리 && 해당 위치까지 거리값을 기록
+                queue.enqueue(next);
+            }
+        }
+    }
+}
+console.log(bfs());
---
+++ baekjoon/7562.js
@@ -0,0 +1,81 @@
+class Queue {
+    constructor() {
+        this.items = [];
+        this.headIndex = 0;
+        this.tailIndex = 0;
+    }
+    enqueue(item) {
+        this.items[this.tailIndex] = item;
+        this.tailIndex++;
+    }
+    dequeue() {
+        const item = this.items[this.headIndex];
+        delete this.items[this.headIndex];
+        this.headIndex++;
+        return item;
+    }
+    peek() {
+        return this.items[this.headIndex];
+    }
+    getLength() {
+        return this.tailIndex - this.headIndex;
+    }
+
+}
+
+const test = "3\n" +
+    "8\n" +
+    "0 0\n" +
+    "7 0\n" +
+    "100\n" +
+    "0 0\n" +
+    "30 50\n" +
+    "10\n" +
+    "1 1\n" +
+    "1 1";
+const input = test.toString().split('\n');
+
+let dx = [-2, -2, -1, -1, 1, 1, 2, 2];
+let dy = [-1, 1, -2, 2, -2, 2, -1, 1];
+
+let testCases = Number(input[0]);
+let line = 1;
+while(testCases--) {
+    let l = Number(input[line]);
+    let [x, y] = input[line+1].split(' ').map(Number);
+    let [targetX, targetY] = input[line+2].split(' ').map(Number);
+
+    // 거리 배열 lxl 크기로 생성
+    let visited = [];
+    for (let i=0; i<l; i++) {
+        visited.push(new Array(l).fill(0));
+    }
+
+    // bfs process
+    // 1. 최초 노드 큐에 넣고, 최초 거리를 1로 두기
+    // 2. 큐 길이가 0 될 때까지 디큐
+    // 3. 디큐하면서 8개 위치 반복문 (반복문 하기 위해 이전 위치가 필요하므로 변수에 저장)
+    // 4. 8개 위치들의 범위가 유효한지 & 방문한적 있는지 체크
+    // 5. 해당 없으면 방문 처리하고 (거리 기록하고) 인큐
+    // 6. 테스트 케이스 마지막 줄에 타겟 위치의 거리값 -1 을 출력
+    let queue = new Queue();
+    queue.enqueue([x, y]);
+    visited[x][y] = 1;
+    while(queue.getLength() != 0) {
+        let cur = queue.dequeue();
+        let x = cur[0];
+        let y = cur[1];
+        for (let i=0; i<8; i++) {
+            let nx = x + dx[i];
+            let ny = y + dy[i];
+            if (nx < 0 || ny < 0 || nx >= l || ny >= l) continue;
+            if (visited[nx][ny] == 0) {
+                visited[nx][ny] = visited[x][y] + 1;
+                queue.enqueue([nx, ny]);
+            }
+        }
+    }
+
+    line += 3;
+    console.log(visited[targetX][targetY] - 1);
+}
