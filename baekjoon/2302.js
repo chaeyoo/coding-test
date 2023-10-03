77cf03b9d6ab7fc437ba559f6571c780ea96ca73
chaeyoo <chaeh0124@gmail.com>
2023-10-04 05:06:31 +0900

    [FEAT] baekjoon 2302

---
+++ baekjoon/2302.js
@@ -0,0 +1,38 @@
+const test = "9\n" +
+    "2\n" +
+    "4\n" +
+    "7";
+const input = test.toString().split('\n');
+let n = Number(input[0]);
+let m = Number(input[1]);
+
+let dp = new Array(41).fill(0);
+dp[0] = 1;
+dp[1] = 1;
+
+function fibo(n){
+    if(dp[n] != 0) {
+        return dp[n]
+    } else {
+        dp[n] = fibo(n-1) + fibo(n-2);
+        return dp[n];
+    }
+}
+
+let arr = [];
+let start = 0;
+for (let i=0; i<m; i++) {
+    let end = Number(input[i+2]);
+    arr.push(end -1 -start);
+    start = end;
+}
+
+arr.push(n - start);
+
+// console.log(arr)
+let res = 1;
+for (let x of arr) {
+    res *= fibo(x);
+}
+
+console.log(res);
