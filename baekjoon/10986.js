55fcb7e9618b865d83ee06cae1dfe13481c6d35e
chaeyoo <chaeh0124@gmail.com>
2023-10-14 11:30:57 +0900

    [FEAT] prefix sum baekjoon 10986

--- baekjoon/10986.js
+++ baekjoon/10986.js
@@ -1,5 +1,7 @@
-const test = "5 3\n" +
-    "1 2 3 1 2";
+// const test = "5 3\n" +
+//     "1 2 3 1 2";
+const test = "10 5\n" +
+    "10 4 7 8 6 9 13 12 21 24";
 const input = test.toString().split('\n');
 
 let [n, m] = input[0].split(' ').map(Number);
@@ -32,14 +34,31 @@ for (let i=0; i<=n; i++) {
 }
 
 // 나머지를 COUNTING
-// console.log(counter);
+console.log(counter);
 
 let result = 0;
 for (let i=0; i<m; i++) {
     // counter[i]개에서 2개를 고르는 조합
     if (i in counter) {
+        console.log(parseInt(counter[i] * (counter[i] - 1)/2))
         result += parseInt(counter[i] * (counter[i] - 1)/2);
     }
 }
 
 console.log(result);
+//
+// ** 해설
+// 조건) 문제 연속 구간의 합이 M으로 나누어 떨어지는 경우의 수 구하기
+// 식: (prefixSum[end] - prefixSum[start-1]) % M == 0 인 개수
+// => prefixSum[end] % M - prefixSum[start-1] % M = 0
+// => prefixSum[end] % M = prefixSum[start-1] % M
+//
+// 풀이)
+// M으로 나누었을 때 나올 수 있는 나머지 별로 카운트를 해줌
+// 그 카운트 한 것 중에 2개를 뽑는 경우의 수를 모두 합해줌
+// ex) { '0': 4, '1': 1, '2': 1, '4': 5 }
+// 누적합들을 5로 나누었을 때 나머지가 0인 경우는 4가지
+// 4개중 2개를 뽑기 => 6가지
+// 누적합들을 5로 나누었을 때 나머지가 4인 경우는 5가지
+// 5개중 2개를 뽑기 => 10가지
+// 합은 16
