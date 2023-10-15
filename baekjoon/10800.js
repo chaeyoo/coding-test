e0f982ecbbe68703b0991cd94049eab7a7e0c090
chaeyoo <chaeh0124@gmail.com>
2023-10-15 21:57:43 +0900

    [FEAT] prefix sum baekjoon 10800

--- baekjoon/10800.js
+++ baekjoon/10800.js
@@ -1,38 +1,61 @@
-const test = "4\n" +
-    "1 10\n" +
-    "3 15\n" +
-    "1 3\n" +
-    "4 8";
+const test = "4\n"
+    + "1 10\n"
+    + "3 15\n"
+    + "1 3\n"
+    + "4 10";
 const input = test.toString().split('\n');
 const n = Number(input[0]);
 let arr = [];
-for (let i=0; i<n; i++) {
-    let c = Number(input[i+1].split(' ')[0]);
-    let s = Number(input[i+1].split(' ')[1]);
+for (let i = 0; i < n; i++) {
+    let c = Number(input[i + 1].split(' ')[0]);
+    let s = Number(input[i + 1].split(' ')[1]);
     arr.push([c, s, i]);
 }
 arr.sort(function (a, b) {
     return a[1] - b[1];
 });
 
+// console.log(arr);
 let sum = 0; // 전체 누적합
-let colorSum = Array(200001).fill(0); // 색상별 누적 합 (최대 20만개)
+let colorSum = Array(5).fill(0); // 색상별 누적 합 (최대 20만개)
 let result = Array(n).fill(0); // 최종결과 배열
 
 let start = 0;
 while (start < n) {
+    console.log('====while START====')
+    console.log(arr);
+    console.log('while >> sum', sum);
+    console.log('while >> colorSum', colorSum);
+    console.log('while >> result', result);
+    console.log(start, 'start');
     let end = start;
+    // 크기가 같은 공의 마지막 인덱스 찾기
+    // 크기가 작은 애들만 잡아먹을 수 있으니까
+    // 크기가 같을 때까지 누적합에 대한 처리가 필요하기 때문에
+    // 아래 while문이 필요함
+    // end가 n보다 작으면서 size가 같을 때까지 end += 1
     while (end < n && arr[start][1] == arr[end][1]) {
         end += 1;
     }
-    for (let i=start; i<end; i++) {
+    console.log(end, 'end');
+    for (let i = start; i < end; i++) {
+        console.log('첫번째 for::, i:::', i);
+        // 크기 순서로 정렬했을 때 i번째의 원래 처음 인덱스 (결과값 저장을 위해)
         result[arr[i][2]] = sum - colorSum[arr[i][0]];
+        console.log('첫번째 for::, result', result);
     }
-    for (let i=start; i<end; i++) {
+
+    for (let i = start; i < end; i++) {
+        console.log('두번째 for::, i:::', i);
+        // 컬러 인덱스에다가 크기값을 누적합
         colorSum[arr[i][0]] += arr[i][1];
+        // 전체 누적합
         sum += arr[i][1];
+        console.log('두번째 for::, colorSum', colorSum);
+        console.log('두번째 for::, sum', sum);
     }
     start = end;
+    console.log('====while END====')
 }
 
 console.log(result.join('\n'));
