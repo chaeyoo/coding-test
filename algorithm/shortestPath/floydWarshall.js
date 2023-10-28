85c9bd8072019dd0e6264dab586bfca362d7e567
chaeyoo <chaeh0124@gmail.com>
2023-10-28 13:14:16 +0900

    [FEAT] Floyd-Warshall algorithm

---
+++ algorithm/shortestPath/floydWarshall.js
@@ -0,0 +1,30 @@
+let INF = 1e9;
+let n = 5;
+
+let graph = [
+    [INF, INF, INF, INF, INF],
+    [INF, 0 , 1, 5, INF, INF],
+    [INF, 7, 0, 2, 2, INF],
+    [INF, 2, INF, 0, INF, 6],
+    [INF, INF, 2, INF, 0, INF],
+    [INF, INF, INF, 1, INF, 0]
+];
+
+// Floyd-Warshall algorithm
+for (let k=1; k<=n; k++) {
+    for (let a=1; a<=n; a++) {
+        for (let b=1; b<=n; b++) {
+            let cost = graph[a][k] + graph[k][b];
+            graph[a][b] = Math.min(graph[a][b], cost);
+        }
+    }
+}
+
+for (let a=1; a<=n; a++) {
+    let line = '';
+    for (let b=1; b<=n; b++) {
+        if (graph[a][b] == INF) line += 'INF ';
+        else line += graph[a][b] + ' ';
+    }
+    console.log(line);
+}
