0aab9efbf374fa00ecc27139e14533bbe0c55488
chaeyoo <chaeh0124@gmail.com>
2024-05-06 17:53:38 +0900

    update 13305

--- type-study/greedy/13305.js
+++ type-study/greedy/13305.js
@@ -9,16 +9,16 @@ const n = Number(input[0]);
 let dis = input[1].split(' ').map(Number);
 let cost = input[2].split(' ').map(Number);
 
-let tmpCost = cost[0]
-let total = 0;
-for (let i = 0; i < n-1 ; i++) {
-    if (tmpCost <= cost[i]) {
-        total += tmpCost * dis[i]
-        tmpCost = Math.min(tmpCost, cost[i+1]);
-    } else {
-        console.log(cost[i] * dis[i])
-        total += cost[i] * dis[i];
-    }
+let minCost = cost[0];
+
+for (let i=0; i<n; i++) {
+    minCost = Math.min(minCost, cost[i]);
+    cost[i] = minCost;
 }
 
-console.log(total)
+let answer = BigInt(0);
+for (let i=0; i<n-1; i++) {
+    answer += BigInt(dis[i]) * BigInt(cost[i]);
+}
+
+console.log(String(answer))
