1e49dbd987dd3750ddd826d02badfa4f33a2542a
chaeyoo <chaeh0124@gmail.com>
2024-10-14 21:13:13 +0900

    [FEAT] codility 2 questions + 1 solution

--- codility/CyclicRotation2.js
+++ codility/CyclicRotation2.js
@@ -1,49 +1,25 @@
-// 틀린 풀이
+
 function solution(A, K) {
     // Implement your solution here
-    // 4, 8, 12 --- 
-    let arrLen = A.length;
-    let rotNum = Number(K) % arrLen;
-    function solution(A, K) {
-        // Implement your solution here
-        const move = K % A.length;
-        if (move) {
-            const newArr = A.splice(A.length - move);
-            return [...newArr, ...A];
-        }
-        return A;
+    const move = K % A.length;
+    if (move) {
+        console.log(A.length - move);
+        // console.log(A.splice(A.length - move));
+        const newArr = A.splice(A.length - move);
+        console.log(newArr);
+        console.log(A);
+        // 자른것과 잘린것을 붙이기
+        return [...newArr, ...A];
     }
-    
-    // console.log(arrLen);
-    // console.log(rotNum);
-    // [3, 8, 9, 7, 6] => [0, 1, 2, 3, 4] 
-    // [9, 7, 6, 3, 8] => [2, 3, 4, 0, 1] 
-
-    // K와 크거나 같은 건 idx%K
-    // K보다 작은 건 idx + K - 1
-
-    if(rotNum === 0) {
-        return A;
-    }
-    let arr = [];
-    for (let i=0; i<arrLen; i++) {
-        if (i >= rotNum) {
-            arr[i] = A[i%rotNum]
-        } else {
-            arr[i] = A[i+rotNum - 1];
-        }
-    }
-
-    // console.log(arr);
-    return arr;
+    return A;
 }
 
-// K를 배열의 길이로 나눈 나머지 만큼 이동
-// console.log(8 % 5);
-// console.log(13 % 5);
-// solution([3, 8, 9, 7, 6], 3);
+// console.log(solution([1, 2, 3, 4], 4));
+console.log(solution([3, 8, 9, 7, 6], 3));
 
-console.log(solution([1, 2, 3, 4], 4));
+// console.log([3, 8, 9, 7, 6].splice(3)); // 인덱스 3, 4
+// console.log([3, 8, 9, 7, 6].splice(2)); // 인덱스 2, 3, 4
+
 
 // Question
 // An array A consisting of N integers is given. Rotation of the array means that each element is shifted right by one index, and the last element of the array is moved to the first place. For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] (elements are shifted right by one index and 6 is moved to the first place).
